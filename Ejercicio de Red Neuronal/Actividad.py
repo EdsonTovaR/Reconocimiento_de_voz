dae75fb605d059d4fe174401f3cfed3a6ae2387d
Edson Tovar <102639181+EdsonTovaR@users.noreply.github.com>
2023-11-04 16:37:40 -0600

    Tarea Terminada

--- Ejercicio de Red Neuronal/Actividad.py
+++ Ejercicio de Red Neuronal/Actividad.py
@@ -42,9 +42,33 @@ model.compile(loss='mean_squared_error',
               optimizer='adam',
               metrics=['binary_accuracy'])
 
-model.fit(training_data, target_data, epochs=1000)
+model.fit(training_data, target_data, epochs=3000)
 
 scores = model.evaluate(training_data, target_data)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 print (model.predict(training_data).round())
+
+
+# serializar el modelo a JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serializar los pesos a HDF5
+model.save_weights("model.h5")
+print("Modelo Guardado!")
+ 
+# mas tarde...
+ 
+# cargar json y crear el modelo
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# cargar pesos al nuevo modelo
+loaded_model.load_weights("model.h5")
+print("Cargado modelo desde disco.")
+ 
+# Compilar modelo cargado y listo para usar.
+loaded_model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
+
