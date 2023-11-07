a04a26d48fdefc2143de19df56d916fee3c59eb6
Edson Tovar <102639181+EdsonTovaR@users.noreply.github.com>
2023-11-06 20:51:58 -0600

    Actividad Terminada

--- Practicas en Clase/actividad1.py
+++ Practicas en Clase/actividad1.py
@@ -1,26 +1,30 @@
 import speech_recognition as sr
 
-#Crea un objeto de reconocimiento de voz
-recognizer=sr.Recognizer()
+def reconocer_voz_y_guardar():
+    # Crear un objeto de reconocimiento de voz
+    r = sr.Recognizer()
 
-#Abre un archivo de texto
-with open("Resultados.txt","w") as f:
-    
-    #Escuchamos al usuario
-    audio = recognizer.listen()
-    
-    #Intentamos reconocer la voz
+    # Configurar el micrófono
+    with sr.Microphone() as source:
+        print("Hable para comenzar a reconocer la voz...")
+        audio = r.listen(source)
+        print("Escuchado, ahora reconociendo...")
+
     try:
-        #Obtiene el texto reconocido
-        text=recognizer.recognize_google(audio)
-        
-        #Imprime el texto reconocido
-        print(text)
-        
-        #Guarda el texto reconocido en el archivo
-        f.write(str(text))
-        
+        # Reconocer la voz usando el reconocedor de voz de Google
+        texto_reconocido = r.recognize_google(audio, language="es-ES")
+        print("Texto reconocido: " + texto_reconocido)
+
+        # Guardar la transcripción en un archivo de texto
+        with open("transcripcion.txt", "a") as archivo:
+            archivo.write(texto_reconocido + "\n")
+        print("Transcripción guardada en 'transcripcion.txt'")
+
     except sr.UnknownValueError:
-        print("No se pudo reconocer la voz")
+        print("No se pudo entender lo que dijiste")
     except sr.RequestError as e:
-        print(f"Error de solicitud: {e}")
+        print("Error al conectarse al servicio de reconocimiento de voz: {0}".format(e))
+
+
+if __name__ == "__main__":
+    reconocer_voz_y_guardar()
